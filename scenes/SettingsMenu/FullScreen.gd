e3e8f10bf53c03093afddef9991535b21565a36c
Birtalan Márk <markbirtalan@gmail.com>
2022-03-01 21:34:32 +0100

    Giving some love

--- scenes/SettingsMenu/FullScreen.gd
+++ scenes/SettingsMenu/FullScreen.gd
@@ -9,6 +9,7 @@ var root = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	root = get_tree().get_nodes_in_group("settings")[0]
+	self.toggle_mode = OS.window_fullscreen
 
 func _on_FullScreen_toggled(button_pressed):
 	root.saveSetting("VideoSettings", "FullScreen", button_pressed)
